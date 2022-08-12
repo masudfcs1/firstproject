e0cde369c34aca43103535eb523b0a029abce682
masudfcs1 <masudfcs1@gmail.com>
2022-08-12 08:17:45 +0600

    First

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -12,6 +12,7 @@
   </head>
   <body>
 
+
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">Laravel Project</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,7 +23,7 @@
           <ul class="navbar-nav mr-auto">
 
             <li class="nav-item">
-              <a class="nav-link" href="#">Home</a>
+              <a class="nav-link" href="/home">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="/contact">contact</a>
@@ -37,7 +38,7 @@
         </div>
       </nav>
 
-
+     <h1 class="">About</h1>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
